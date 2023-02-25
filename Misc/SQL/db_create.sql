f63f54f48f911a828064143936c35402ab84135d
tovDmitrij <DMXikka@gmail.com>
2023-02-25 20:09:59 +0300

    Патч - Обновил описание проекта, добавив схему архитектуры приложения, описание таблиц БД и описание возможностей приложения; - В планах добавить установщик в виде Docker.

---
+++ Misc/SQL/db_create.sql
@@ -0,0 +1,6 @@
+﻿CREATE DATABASE "TradingCompany"
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
---
+++ Misc/SQL/db_jobs.sql
@@ -0,0 +1,49 @@
+﻿--Обновление курса валют (ежедневно)
+DO $$
+	DECLARE
+		jid integer;
+		scid integer;
+	BEGIN
+		INSERT INTO pgagent.pga_job(jobjclid, jobname, jobdesc, jobhostagent, jobenabled) 
+		VALUES (1::integer, 'UpdateCources'::text, ''::text, ''::text, true) 
+		RETURNING jobid INTO jid;
+
+		INSERT INTO pgagent.pga_jobstep (jstjobid, jstname, jstenabled, jstkind, jstconnstr, jstdbname, jstonerror, jstcode, jstdesc) 
+		VALUES (jid, 'step1'::text, true, 's'::character(1), ''::text, 'TradingCompany'::name, 'f'::character(1), 'call UpdateCources();'::text, ''::text);
+
+		INSERT INTO pgagent.pga_schedule(jscjobid, jscname, jscdesc, jscenabled, jscstart, jscminutes, jschours, jscweekdays, jscmonthdays, jscmonths) 
+		VALUES (jid, 'schedule1'::text, ''::text, true,
+			'2023-02-26 19:44:00+03'::timestamp with time zone, 
+			ARRAY[true,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false]::boolean[],
+			ARRAY[true,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false]::boolean[],
+			ARRAY[true,true,true,true,true,true,true]::boolean[],
+			ARRAY[false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false]::boolean[],
+			ARRAY[false,false,false,false,false,false,false,false,false,false,false,false]::boolean[]) 
+		RETURNING jscid INTO scid;
+	END 
+$$;
+
+--Обновление цен товаров (еженедельно)
+DO $$
+	DECLARE
+		jid integer;
+		scid integer;
+	BEGIN
+		INSERT INTO pgagent.pga_job(jobjclid, jobname, jobdesc, jobhostagent, jobenabled) 
+		VALUES (1::integer, 'UpdatePrices'::text, ''::text, ''::text, true) 
+		RETURNING jobid INTO jid;
+
+		INSERT INTO pgagent.pga_jobstep (jstjobid, jstname, jstenabled, jstkind, jstconnstr, jstdbname, jstonerror, jstcode, jstdesc) 
+		VALUES (jid, 'step1'::text, true, 's'::character(1), ''::text, 'TradingCompany'::name, 'f'::character(1), 'call UpdatePrices();'::text, ''::text);
+
+		INSERT INTO pgagent.pga_schedule(jscjobid, jscname, jscdesc, jscenabled, jscstart, jscminutes, jschours, jscweekdays, jscmonthdays, jscmonths) 
+		VALUES (jid, 'schedule1'::text, ''::text, true,
+			'2023-02-26 19:50:00 +03:00'::timestamp with time zone, 
+			ARRAY[false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false]::boolean[],
+			ARRAY[false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false]::boolean[],
+			ARRAY[true,false,false,false,false,false,false]::boolean[],
+			ARRAY[false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false]::boolean[],
+			ARRAY[false,false,false,false,false,false,false,false,false,false,false,false]::boolean[]) 
+		RETURNING jscid INTO scid;
+	END
+$$;
